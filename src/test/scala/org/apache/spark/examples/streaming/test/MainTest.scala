f31eda0c30046bf653a2ff4bfd14981317cf5316
fxb <punkflyer@gmail.com>
2018-05-21 17:19:17 +0800

    add udf

--- src/test/scala/org/apache/spark/examples/streaming/test/MainTest.scala
+++ src/test/scala/org/apache/spark/examples/streaming/test/MainTest.scala
@@ -3,9 +3,9 @@ package org.apache.spark.examples.streaming.test
 import java.util.UUID
 
 import org.apache.spark.examples.streaming.test.FieldFilterTest.registerDF
-import org.apache.spark.examples.streaming.utils.StructTypeConverter
+import org.apache.spark.examples.streaming.utils.{CommonFilter, StructTypeConverter}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{from_json, udf}
+import org.apache.spark.sql.functions.{from_json, typedLit, udf}
 
 object MainTest {
 
@@ -79,31 +79,23 @@ object MainTest {
       }
     })
 
-    //自定义udf
-    val code = (arg: Any) => {
-      arg.getClass.getName
+    val code = (field: String, map: Map[String, String]) => {
+      CommonFilter.filter(field, map)
     }
-    val addCol = udf(code)
+    val updateCol = udf(code)
 
     df1.columns.foreach(fieldSet => {
-      df2 = df2.withColumn(fieldSet, addCol(df2.col(fieldSet)))
+      val sql = spark.sql("select * from fieldSet f, range r where f.FieldRange = r.FieldRangeType")
+      if (sql.collect().size > 0) {
+        val map = sql.rdd.map(row => (row.getAs("FieldRangeValue").toString, row.getAs("FieldRange").toString))
+          .collectAsMap()
+        df2 = df2.withColumn(fieldSet, updateCol(df2.col(fieldSet), typedLit(map)))
+      }
     })
 
     df2.writeStream
       .outputMode("append")
       .format("console")
-      //      .foreach(new ForeachWriter[Row] {
-      //        override def open(partitionId: Long, version: Long): Boolean = true
-      //
-      //        override def process(value: Row): Unit = {
-      ////          value.schema.fieldNames.foreach(field => {
-      ////            println(CommonFilter.filter(value.getAs(field).toString, map))
-      ////          })
-      //
-      //        }
-      //
-      //        override def close(errorOrNull: Throwable): Unit = {}
-      //      })
       .option("checkpointLocation", "/tmp/temporary-" + UUID.randomUUID.toString)
       .start().awaitTermination()
   }
