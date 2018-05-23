fd3857a8d047d73f8af1771544bc8ee13cd739f3
fxb <punkflyer@gmail.com>
2018-05-23 10:39:10 +0800

    移动测试类

--- src/test/scala/org/apache/spark/examples/streaming/test/Main.scala
+++ src/test/scala/org/apache/spark/examples/streaming/test/Main.scala
@@ -1,43 +1,7 @@
 package org.apache.spark.examples.streaming.test
 
-import org.apache.spark.examples.streaming.test.FieldFilterTest.registerDF
-import org.apache.spark.sql.SparkSession
-
 object Main {
 
-  def main(args: Array[String]): Unit = {
-
-    val spark = SparkSession.builder.
-      master("local[*]")
-      .appName("StructuredStreamingTest")
-      .getOrCreate
-
-    val tableName = "t_jc_cljbxx"
-
-    registerDF(spark, "gathertables").createOrReplaceTempView("table")
-    val table = spark.sql("select * from table")
-    registerDF(spark, "gatherfieldset").createOrReplaceTempView("fieldSet")
-    val fieldSet = spark.sql("select * from fieldSet f where f.IsGather = true and f.TableId = 17")
-    fieldSet.show
-
-    registerDF(spark, "fieldrangeinfo").createOrReplaceTempView("range")
-    val range = spark.sql("select * from range")
-
-    val map = spark.sql(s"select FieldRangeValue,FieldRange from range")
-      .rdd.map(row => (row.getAs("FieldRangeValue").toString, row.getAs("FieldRange").toString))
-      .collectAsMap()
-    map.foreach(println)
-
-
-    val cleangrange = spark.sql("select FieldRangeType, concat_ws(',',collect_set(FieldRangeValue)) as values from range group by FieldRangeType")
-    cleangrange.show
-    cleangrange.rdd.foreach(println)
-
-    val joinedFieldSet = fieldSet.join(cleangrange, fieldSet("FieldRange") === range("FieldRangeType"), "left").select("FieldName", "FieldRangeType", "values")
-    joinedFieldSet.show
-
-    //    val map = joinedFieldSet.rdd.map(row => (row.getAs[String]("Id"), row.getAs[String]("FieldRange"))).collectAsMap()
-    //    map.foreach(println)
-  }
+  def main(args: Array[String]): Unit = {}
 
 }
