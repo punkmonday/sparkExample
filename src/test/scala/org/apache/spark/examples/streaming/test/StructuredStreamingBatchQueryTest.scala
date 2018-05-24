0007e49b152c591279983a856cc7a28997da410e
fxb <punkflyer@gmail.com>
2018-05-24 20:08:23 +0800

    add batch query test

--- src/test/scala/org/apache/spark/examples/streaming/test/StructuredStreamingBatchQueryTest.scala
+++ src/test/scala/org/apache/spark/examples/streaming/test/StructuredStreamingBatchQueryTest.scala
@@ -1,21 +1,18 @@
 package org.apache.spark.examples.streaming.test
 
-import java.util.UUID
-
 import org.apache.spark.sql.SparkSession
 
 /**
   * 此程序演示spark structure streaming获取kafka stream并输出到控制台
   *
   */
-object StructuredStreamingTest {
+object StructuredStreamingBatchQueryTest {
 
   def main(args: Array[String]): Unit = {
     //初始化sparkSession
     val spark = SparkSession.builder.master("local[*]").appName("StructuredStreamingTest").getOrCreate()
     //隐式导入，必须，否则会报错
-    import spark.implicits._
-    spark.readStream
+    spark.read
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "test")
@@ -23,10 +20,8 @@ object StructuredStreamingTest {
       .load()
       .selectExpr("CAST(value AS STRING)")
       .as[String]
-      .writeStream
-      .outputMode("append")
+      .write
       .format("console")
-      .option("checkpointLocation", "/tmp/temporary-" + UUID.randomUUID.toString)
-      .start().awaitTermination()
+      .save
   }
 }
