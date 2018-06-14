02e3970b00537dd27a54943a59834846c08f7a7e
fxb <punkflyer@gmail.com>
2018-06-14 17:29:56 +0800

    update logic

--- src/main/scala/org/apache/spark/examples/streaming/utils/StructTypeConverter.scala
+++ src/main/scala/org/apache/spark/examples/streaming/utils/StructTypeConverter.scala
@@ -7,25 +7,29 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object StructTypeConverter {
 
   def convert(df: DataFrame): StructType = {
-    val structFields = df.rdd.filter(row => row.getAs("IsGather").toString.toBoolean).map(row =>
+    val structFields = df.rdd.map(row =>
       StructField(
         row.getAs("FieldName").toString,
-        row.getAs("DataType").toString match {
+        row.getAs("DataType").toString.toUpperCase match {
           case "VARCHAR" => StringType
+          case "VARCHAR2" => StringType
+          case "TEXT" => StringType
           case "BIGINT" => IntegerType
           case "INT" => IntegerType
           case "BOOLEAN" => BooleanType
+          case "DATETIME" => DateType
+          case "DATE" => DateType
+          case "DOUBLE" => DoubleType
+          case "TIMESTAMP" => TimestampType
           case _ => StringType
         },
         row.getAs("IsEmpty")
       )
     ).collect()
-
     new StructType(structFields)
   }
 
   def main(args: Array[String]): Unit = {
-
     val spark = SparkSession.builder.master("local[*]").appName("StructTypeConverter").getOrCreate()
     val df = spark.read
       .format(PropertiesUtil.getProperty("config.sql.format"))
